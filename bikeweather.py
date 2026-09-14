74f074ac1bf6e8563aae795ca255d5717c863cec
Hattum Smit <40392927+hattum@users.noreply.github.com>
2026-09-14 16:57:30 +0200

    Update bikeweather.py

--- bikeweather.py
+++ bikeweather.py
@@ -303,12 +303,12 @@ def main():
 
     now = datetime.now(LOCAL_TIMEZONE)
 
-    if now.hour not in (7, 15):
-        print(
-            f"Current Amsterdam time is {now:%H:%M}. "
-            "Not a scheduled notification time. Exiting."
-        )
-        return
+    # if now.hour not in (7, 15):
+    #     print(
+    #         f"Current Amsterdam time is {now:%H:%M}. "
+    #         "Not a scheduled notification time. Exiting."
+    #     )
+    #     return
 
     print("Starting bike weather check...")
     print()
